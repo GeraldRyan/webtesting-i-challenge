1d26306b0cf28b35c0a7b998bb728226f983e64f
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-20 18:14:17 -0500

    mvp

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -52,15 +52,14 @@ describe('REPAIR()', ()=>{
 })
 
 
-
-// describe('GET()', ()=>{
-//   it('should accept an item object and return a new item object modified by rules ordained by client for enhancement success', ()=>{
-//     //setup
-//     let expected = {...testItem, enhancement: 20}
-//     let initial = {...testItem, enhancement: 19}
-//     //execute
-//     let actual = enhancer.succeed(initial)
-//     //check
-//     expect(actual.enhancement).toBe(expected.enhancement)
-//   })
-// })
+describe('GET()', ()=>{
+  it('should accept an item object and return a new item object modified by rules ordained by client for enhancement success', ()=>{
+    //setup
+    let expected = {...testItem, enhancement: 20}
+    let initial = {...testItem, enhancement: 19}
+    //execute
+    let actual = enhancer.succeed(initial)
+    //check
+    expect(actual.enhancement).toBe(expected.enhancement)
+  })
+})
