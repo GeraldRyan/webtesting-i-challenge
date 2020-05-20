ab916d71991aa28dd6929d635651dff5d2b2ea7d
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-20 17:45:29 -0500

    not far

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,8 +5,14 @@ module.exports = {
   get,
 };
 
+// let item = {
+//   name:0,
+//   durability:0,
+//   enhancement:0
+// }
+
 function succeed(item) {
-  return { ...item };
+  return { ...item, enhancement: item.enhancement++ };
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+let testItem = {
+  name:"club",
+  durability:0,
+  enhancement:0
+}
+
+describe('succeed()', ()=>{
+  it('should accept an item object and return a new item object modified by rules ordained by client for enhancement success', ()=>{
+    //setup
+    let expected = {...testItem, enhancement: 5}
+
+    let initial = {...testItem, enhancement: 4}
+    //execute
+    let actual = enhancer.succeed(initial)
+    //check
+    expect(actual).toBe(expected)
+  })
+})
